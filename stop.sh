ae35f6e275a47086ee2d0abca6c505821648ee76
Joao <pestanapt@gmail.com>
2025-07-22 21:45:52 +0100

    added sudo

--- stop.sh
+++ stop.sh
@@ -8,7 +8,7 @@ SERVICES=("piHole")
 
 for SERVICE in "${SERVICES[@]}"; do
   echo "⏹️ Parando $SERVICE..."
-  docker compose -f "docker/$SERVICE/docker-compose.yml" down
+  sudo docker compose -f "docker/$SERVICE/docker-compose.yml" down
 done
 
 echo "✅ Todos os serviços foram parados!"
