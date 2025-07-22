e17bf232d619c6c05a5bf22cc9937d06f16cdcd1
Joao <pestanapt@gmail.com>
2025-07-22 21:11:23 +0100

    added initialize script

---
+++ initialize.sh
@@ -0,0 +1,10 @@
+echo "🟢 A iniciar os serviços..."
+
+SERVICES=("piHole")
+
+for SERVICE in "${SERVICES[@]}"; do
+  echo "➡️ Subindo $SERVICE..."
+  sudo docker compose -f "$SERVICE/docker-compose.yml" up -d
+done
+
+echo "✅ Todos os serviços foram iniciados!"
